cbb1745371212bd0422cbd7635a2eeb5ebd6b9df
trodenbeck <trodenbeck@cherrycreekschools.org>
2022-10-14 10:29:28 -0600

    2.4

---
+++ Challenge 2.4/Assets/Scripts/Practice.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    bool PracticeVar = true;
+    Rigidbody2D PracticeObject = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PracticeVar);
+        Debug.Log(PracticeObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
